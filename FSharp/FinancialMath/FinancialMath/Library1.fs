7f94c06e979a1d84e07278ba7032a30d4edce02f
yunlinz <yunlinz@gmail.com>
2015-07-10 23:31:40 -0400

    included fsx testing code for finmath

--- FSharp/FinancialMath/FinancialMath/Library1.fs
+++ FSharp/FinancialMath/FinancialMath/Library1.fs
@@ -26,3 +26,19 @@ module PathGenerator =
         path |> Array.mapi (fun n p -> ((float n) * dt, p))
              |> Chart.Line
              |> Chart.WithStyling (Color = color, BorderWidth = 2)
+
+    let S_T (path:float array) = path.[path.Length - 1]
+
+    let european_call K (path: float array) = max ((S_T path) - K) 0.0
+
+    let up_and_out_call K H (path:float array) = 
+        if Array.max path.[1..] >= H then 0.0
+        else european_call K path
+
+    let simulate_payoffs rnd S0 r sigma T N M payoff = 
+        [| for path in generate_GBM_paths_by_log rnd S0 r sigma T N M ->
+                let currentPayoff = payoff path
+                (exp(-r*T)) * currentPayoff  |]
+
+    let price_option rnd S0 r sigma T N M payoff = 
+        simulate_payoffs rnd S0 r sigma T N M payoff |> Array.average
---
+++ FSharp/FinancialMath/FinancialMath/Script.fsx
@@ -0,0 +1,48 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+#r @"..\packages\MathNet.Numerics.3.7.0\lib\net40\MathNet.Numerics.dll"
+#r "System.Windows.Forms.DataVisualization.dll"
+#r @"..\packages\FSharp.Charting.0.90.12\lib\net40\FSharp.Charting.dll"
+#r "System.Drawing.dll"
+
+#load "Library1.fs"
+open FinancialMath.PathGenerator
+open FSharp.Charting
+open System.Drawing
+open System
+
+let K = 100.
+let T = 0.25
+let M = 10000000
+let N = 1
+let payoff = european_call K
+let S0 = 100.0
+let sigma = 0.4
+let r = 0.02
+let rnd = (new System.Random())
+
+let C = price_option rnd S0 r sigma T N M payoff
+
+//let paths = FinancialMath.PathGenerator.generate_GBM_paths_by_log rnd S0 r sigma T N M
+//
+//let mx, mn = paths |> Array.fold (fun (mx, mn) p -> 
+//                                                    (max mx (Array.max p), min mn (Array.min p)))
+//                                                    (Double.MinValue, Double.MaxValue)
+//
+//let colors = [| for i in 1..M -> Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256)) |]
+//
+//let path_charts = Array.map2 (plot_path T N) paths colors
+//
+//let title = sprintf 
+//                "3 simulated GBM paths"
+//
+//let chart = Chart.Combine path_charts
+//            |> Chart.WithStyling(Margin =( 2.0, 12.0, 2.0, 2.0))
+//            |> Chart.WithTitle (Text = title, FontName = "Arial", FontSize = 14.0, FontStyle = FontStyle.Bold, InsideArea = false)
+//            |> Chart.WithXAxis(Title = "time (yr)", Max = T, Min = 0.0, TitleAlignment = StringAlignment.Center, TitleFontName = "Arial", TitleFontStyle = FontStyle.Bold)
+//            |> Chart.WithYAxis(Title = "price $", Max = (Math.Round(mx) + 1.0), Min = (Math.Round(mn) - 1.0), TitleAlignment = StringAlignment.Center, TitleFontName = "Arial", TitleFontStyle = FontStyle.Bold)
+//
+//chart.ShowChart()
+
+// Define your library scripting code here
+
